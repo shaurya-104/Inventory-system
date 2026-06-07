6fbca728e061700ab893cab14eb6d82f82fe8d6a
Shaurya <shauryaadityasinghthakur@gmail.com>
2026-06-07 12:59:15 +0530

    added the table to system

--- admin.js
+++ admin.js
@@ -4,19 +4,29 @@ fetch('/api/inventory')
             throw new Error('Item data is not retrived')
         }
         else{
-        Response.json()
+            return Response.json()
     }
 })
     .then(data=>{
         const Inventory_data = data.Inventory_data
         const user_data = data.user_data
         const sell_logs =data.sell_logs
+        const table = document.getElementById("inventoryTable")
     
     Inventory_data.array.forEach(item => {
-        const item_name = item.item_name
-        const id_itemn = item.id
-        const purchase_price = item.purchase_price
-        const listing_price = item.listing_price 
+            const newRow = table.insertRow(-1);
+
+            const cellId = newRow.insertCell(0);
+            const cellName = newRow.insertCell(1);
+            const cellQuantity = newRow.insertCell(2);
+            const cellPurchase = newRow.insertCell(3);
+            const cellListing = newRow.insertCell(4);
+
+            cellId.innerHTML = item.id;
+            cellName.innerHTML = item.item_name;
+            cellQuantity.innerHTML = item.quantity;
+            cellPurchase.innerHTML = "$" + item.purchase_price;
+            cellListing.innerHTML = "$" + item.listing_price;
     });
     user.data.forEach.forEach(item=>{
         const ussername = item.user_name
@@ -27,7 +37,7 @@ fetch('/api/inventory')
         const sold_item = item.item_name
         const selling_price = item.selling_price 
     })
-    })
+})
 
 
         
